ceac05868f8ce67c6ebc7959de211bb9086fee02
Josh Boudreau <jboud1217@gmail.com>
2017-02-02 17:34:29 -0500

    google completed orders conversion script

--- resources/views/admin/orders/page.blade.php
+++ resources/views/admin/orders/page.blade.php
@@ -6,7 +6,7 @@
 
 	@if($status = session('status') && $amount = session('amount'))
 		@if($status == 'completed')
-			<!-- Google Code for Orders Completed Conversion Page -->
+			<!-- Google Code for Completed Orders -->
 			<script type="text/javascript">
 				/* <![CDATA[ */
 				var google_conversion_id = 976131144;
@@ -14,7 +14,7 @@
 				var google_conversion_format = "3";
 				var google_conversion_color = "ffffff";
 				var google_conversion_label = "-MG0CPm16WoQyKi60QM";
-				var google_conversion_value = {{($amount*.05)}};
+				var google_conversion_value = <? echo $amount*.05 ?>;
 				var google_conversion_currency = "USD";
 				var google_remarketing_only = false;
 				/* ]]> */
@@ -23,7 +23,7 @@
 			</script>
 			<noscript>
 				<div style="display:inline;">
-					<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/976131144/?value=50.00&amp;currency_code=USD&amp;label=-MG0CPm16WoQyKi60QM&amp;guid=ON&amp;script=0"/>
+					<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/976131144/?value=<? echo $amount*.05 ?>&amp;currency_code=USD&amp;label=-MG0CPm16WoQyKi60QM&amp;guid=ON&amp;script=0"/>
 				</div>
 			</noscript>
 		@endif
