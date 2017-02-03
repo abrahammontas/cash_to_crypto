b29462b8928a9ac6766d897d33e36970e43b0009
Abraham Montas <abrahammontas@gmail.com>
2017-02-03 22:33:48 +0100

    Changing TZ to America/New_York

--- database/migrations/2017_02_03_203918_alter_table_change_timezone_to_est.php
+++ database/migrations/2017_02_03_203918_alter_table_change_timezone_to_est.php
@@ -18,29 +18,29 @@ class AlterTableChangeTimezoneToEst extends Migration
      */
     public function up()
     {
-        DB::update("UPDATE banks set created_at = CONVERT_TZ(created_at, 'UTC', 'EST')");
-        DB::update("UPDATE banks set updated_at = CONVERT_TZ(updated_at, 'UTC', 'EST')");
+        DB::update("UPDATE banks set created_at = CONVERT_TZ(created_at, 'UTC', 'America/New_York')");
+        DB::update("UPDATE banks set updated_at = CONVERT_TZ(updated_at, 'UTC', 'America/New_York')");
 
-        DB::update("UPDATE orders set created_at = CONVERT_TZ(created_at, 'UTC', 'EST')");
-        DB::update("UPDATE orders set updated_at = CONVERT_TZ(updated_at, 'UTC', 'EST')");
-        DB::update("UPDATE orders set img_updated_at = CONVERT_TZ(img_updated_at, 'UTC', 'EST')");
-        DB::update("UPDATE orders set completed_at = CONVERT_TZ(completed_at, 'UTC', 'EST')");
+        DB::update("UPDATE orders set created_at = CONVERT_TZ(created_at, 'UTC', 'America/New_York')");
+        DB::update("UPDATE orders set updated_at = CONVERT_TZ(updated_at, 'UTC', 'America/New_York')");
+        DB::update("UPDATE orders set img_updated_at = CONVERT_TZ(img_updated_at, 'UTC', 'America/New_York')");
+        DB::update("UPDATE orders set completed_at = CONVERT_TZ(completed_at, 'UTC', 'America/New_York')");
 
-        DB::update("UPDATE password_resets set created_at = CONVERT_TZ(created_at, 'UTC', 'EST')");
+        DB::update("UPDATE password_resets set created_at = CONVERT_TZ(created_at, 'UTC', 'America/New_York')");
 
-        DB::update("UPDATE settings set created_at = CONVERT_TZ(created_at, 'UTC', 'EST')");
-        DB::update("UPDATE settings set updated_at = CONVERT_TZ(updated_at, 'UTC', 'EST')");
+        DB::update("UPDATE settings set created_at = CONVERT_TZ(created_at, 'UTC', 'America/New_York')");
+        DB::update("UPDATE settings set updated_at = CONVERT_TZ(updated_at, 'UTC', 'America/New_York')");
 
-        DB::update("UPDATE surveys set created_at = CONVERT_TZ(created_at, 'UTC', 'EST')");
-        DB::update("UPDATE surveys set updated_at = CONVERT_TZ(updated_at, 'UTC', 'EST')");
+        DB::update("UPDATE surveys set created_at = CONVERT_TZ(created_at, 'UTC', 'America/New_York')");
+        DB::update("UPDATE surveys set updated_at = CONVERT_TZ(updated_at, 'UTC', 'America/New_York')");
 
-        DB::update("UPDATE user_activations set created_at = CONVERT_TZ(created_at, 'UTC', 'EST')");
+        DB::update("UPDATE user_activations set created_at = CONVERT_TZ(created_at, 'UTC', 'America/New_York')");
 
-        DB::update("UPDATE users set created_at = CONVERT_TZ(created_at, 'UTC', 'EST')");
-        DB::update("UPDATE users set updated_at = CONVERT_TZ(updated_at, 'UTC', 'EST')");
+        DB::update("UPDATE users set created_at = CONVERT_TZ(created_at, 'UTC', 'America/New_York')");
+        DB::update("UPDATE users set updated_at = CONVERT_TZ(updated_at, 'UTC', 'America/New_York')");
 
-        DB::update("UPDATE wallets set created_at = CONVERT_TZ(created_at, 'UTC', 'EST')");
-        DB::update("UPDATE wallets set updated_at = CONVERT_TZ(updated_at, 'UTC', 'EST')");
+        DB::update("UPDATE wallets set created_at = CONVERT_TZ(created_at, 'UTC', 'America/New_York')");
+        DB::update("UPDATE wallets set updated_at = CONVERT_TZ(updated_at, 'UTC', 'America/New_York')");
 
     }
 
@@ -51,28 +51,28 @@ class AlterTableChangeTimezoneToEst extends Migration
      */
     public function down()
     {
-        DB::update("UPDATE banks set created_at = CONVERT_TZ(created_at, 'EST', 'UTC')");
-        DB::update("UPDATE banks set updated_at = CONVERT_TZ(updated_at, 'EST', 'UTC')");
+        DB::update("UPDATE banks set created_at = CONVERT_TZ(created_at, 'America/New_York', 'UTC')");
+        DB::update("UPDATE banks set updated_at = CONVERT_TZ(updated_at, 'America/New_York', 'UTC')");
 
-        DB::update("UPDATE orders set created_at = CONVERT_TZ(created_at, 'EST', 'UTC')");
-        DB::update("UPDATE orders set updated_at = CONVERT_TZ(updated_at, 'EST', 'UTC')");
-        DB::update("UPDATE orders set img_updated_at = CONVERT_TZ(img_updated_at, 'EST', 'UTC')");
-        DB::update("UPDATE orders set completed_at = CONVERT_TZ(completed_at, 'EST', 'UTC')");
+        DB::update("UPDATE orders set created_at = CONVERT_TZ(created_at, 'America/New_York', 'UTC')");
+        DB::update("UPDATE orders set updated_at = CONVERT_TZ(updated_at, 'America/New_York', 'UTC')");
+        DB::update("UPDATE orders set img_updated_at = CONVERT_TZ(img_updated_at, 'America/New_York', 'UTC')");
+        DB::update("UPDATE orders set completed_at = CONVERT_TZ(completed_at, 'America/New_York', 'UTC')");
 
-        DB::update("UPDATE password_resets set created_at = CONVERT_TZ(created_at, 'EST', 'UTC')");
+        DB::update("UPDATE password_resets set created_at = CONVERT_TZ(created_at, 'America/New_York', 'UTC')");
 
-        DB::update("UPDATE settings set created_at = CONVERT_TZ(created_at, 'EST', 'UTC')");
-        DB::update("UPDATE settings set updated_at = CONVERT_TZ(updated_at, 'EST', 'UTC')");
+        DB::update("UPDATE settings set created_at = CONVERT_TZ(created_at, 'America/New_York', 'UTC')");
+        DB::update("UPDATE settings set updated_at = CONVERT_TZ(updated_at, 'America/New_York', 'UTC')");
 
-        DB::update("UPDATE surveys set created_at = CONVERT_TZ(created_at, 'EST', 'UTC')");
-        DB::update("UPDATE surveys set updated_at = CONVERT_TZ(updated_at, 'EST', 'UTC')");
+        DB::update("UPDATE surveys set created_at = CONVERT_TZ(created_at, 'America/New_York', 'UTC')");
+        DB::update("UPDATE surveys set updated_at = CONVERT_TZ(updated_at, 'America/New_York', 'UTC')");
 
-        DB::update("UPDATE user_activations set created_at = CONVERT_TZ(created_at, 'EST', 'UTC')");
+        DB::update("UPDATE user_activations set created_at = CONVERT_TZ(created_at, 'America/New_York', 'UTC')");
 
-        DB::update("UPDATE users set created_at = CONVERT_TZ(created_at, 'EST', 'UTC')");
-        DB::update("UPDATE users set updated_at = CONVERT_TZ(updated_at, 'EST', 'UTC')");
+        DB::update("UPDATE users set created_at = CONVERT_TZ(created_at, 'America/New_York', 'UTC')");
+        DB::update("UPDATE users set updated_at = CONVERT_TZ(updated_at, 'America/New_York', 'UTC')");
 
-        DB::update("UPDATE wallets set created_at = CONVERT_TZ(created_at, 'EST', 'UTC')");
-        DB::update("UPDATE wallets set updated_at = CONVERT_TZ(updated_at, 'EST', 'UTC')");
+        DB::update("UPDATE wallets set created_at = CONVERT_TZ(created_at, 'America/New_York', 'UTC')");
+        DB::update("UPDATE wallets set updated_at = CONVERT_TZ(updated_at, 'America/New_York', 'UTC')");
     }
 }
